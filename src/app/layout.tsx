d59dfb6e6d777fbf8b81bee942620df1d85a67a4
adlmii <aidilfahmi601@gmail.com>
2025-12-31 18:48:43 +0700

    Refactor: UI

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ const inter = Inter({
 });
 
 export const metadata: Metadata = {
-  title: "Dil | Frontend Engineer",
+  title: "Aidil | Portfolio",
   description: "Portfolio of a Senior Frontend Engineer",
 };
 
